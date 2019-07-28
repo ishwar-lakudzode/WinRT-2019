8bd5d12bf2260dfad99ce486c153444eebe187fb
ishwar-lakudzode <ishwarnlakudzode@gmail.com>
2019-07-28 17:41:44 +0530

    Assignment 2

---
+++ Win32FirstProgram/Win32FirstProgram/FirstProgram.cpp
@@ -0,0 +1,55 @@
+#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR csCmd, int iShow)
+{
+	WNDCLASSEX wndClass;
+	MSG msg;
+	HWND hWnd;
+	TCHAR szAppName[] = TEXT("FirstWin32Prog");
+
+	wndClass.cbClsExtra = 0;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.cbWndExtra = 0;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.lpszClassName = szAppName;
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!RegisterClassEx(&wndClass))
+	{
+		MessageBox(NULL, TEXT("Error in window creation"), TEXT("Error"), MB_OK);
+		return 0;
+	}
+
+	hWnd = CreateWindow(szAppName, TEXT("First Win32 Program"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance , NULL);
+
+	UpdateWindow(hWnd);
+	ShowWindow(hWnd, iShow);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) 
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
